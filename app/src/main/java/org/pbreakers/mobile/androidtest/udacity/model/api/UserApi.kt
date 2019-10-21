47fd2b791d73e6c249fac54dce13ae6ef7bf5f78
Eric Ampire <ericampire.top@gmail.com>
2019-10-21 08:31:38 +0200

    [UPDATE] UserApi.kt

---
+++ app/src/main/java/org/pbreakers/mobile/androidtest/udacity/model/api/UserApi.kt
@@ -0,0 +1,11 @@
+package org.pbreakers.mobile.androidtest.udacity.model.api
+
+import kotlinx.coroutines.Deferred
+import org.pbreakers.mobile.androidtest.udacity.model.entity.GithubUser
+import retrofit2.http.GET
+
+interface UserApi {
+
+    @GET("/users")
+    fun getAllAsync(): Deferred<List<GithubUser>>
+}
