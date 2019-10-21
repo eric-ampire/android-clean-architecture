030d612359b9803c17b60fe329660e8cc524536d
Eric Ampire <ericampire.top@gmail.com>
2019-10-21 08:30:55 +0200

    [UPDATE] Utils class to manage the loading state

---
+++ app/src/main/java/org/pbreakers/mobile/androidtest/udacity/utils/LoadingState.kt
@@ -0,0 +1,18 @@
+package org.pbreakers.mobile.androidtest.udacity.utils
+
+
+
+@Suppress("DataClassPrivateConstructor")
+data class LoadingState private constructor(val status: Status, val msg: String? = null) {
+    companion object {
+        val LOADED = LoadingState(Status.SUCCESS)
+        val LOADING = LoadingState(Status.RUNNING)
+        fun error(msg: String?) = LoadingState(Status.FAILED, msg)
+    }
+
+    enum class Status {
+        RUNNING,
+        SUCCESS,
+        FAILED
+    }
+}
