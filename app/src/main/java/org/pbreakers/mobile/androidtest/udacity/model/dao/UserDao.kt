91abd972b99a4da8b026b9a38d5383f90d605ca2
Eric Ampire <ericampire.top@gmail.com>
2019-10-21 09:08:08 +0200

    [UPDATE] Conflict strategy

--- app/src/main/java/org/pbreakers/mobile/androidtest/udacity/model/dao/UserDao.kt
+++ app/src/main/java/org/pbreakers/mobile/androidtest/udacity/model/dao/UserDao.kt
@@ -1,15 +1,18 @@
 package org.pbreakers.mobile.androidtest.udacity.model.dao
 
 import androidx.lifecycle.LiveData
+import androidx.room.Dao
 import androidx.room.Insert
+import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import org.pbreakers.mobile.androidtest.udacity.model.entity.GithubUser
 
+@Dao
 interface UserDao {
 
     @Query("SELECT * FROM users")
     fun findAll(): LiveData<List<GithubUser>>
 
-    @Insert
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
     fun add(users: List<GithubUser>)
 }
