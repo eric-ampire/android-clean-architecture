c3af9511d4d085f3a028d9d331cc2bfc929981dd
Eric Ampire <ericampire.top@gmail.com>
2019-10-21 08:27:13 +0200

    [UPDATE] Starting Koin

--- app/src/main/java/org/pbreakers/mobile/androidtest/udacity/app/App.kt
+++ app/src/main/java/org/pbreakers/mobile/androidtest/udacity/app/App.kt
@@ -3,10 +3,20 @@ package org.pbreakers.mobile.androidtest.udacity.app
 import android.app.Application
 import android.content.Context
 import androidx.multidex.MultiDex
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
+import org.koin.core.context.startKoin
+import org.koin.core.logger.Level
+import org.pbreakers.mobile.androidtest.udacity.app.di.*
 
 class App : Application() {
     override fun onCreate() {
         super.onCreate()
+        startKoin {
+            androidContext(this@App)
+            androidLogger(Level.DEBUG)
+            modules(listOf(viewModelModule, repositoryModule, netModule, apiModule, databaseModule))
+        }
     }
 
     override fun attachBaseContext(base: Context?) {
