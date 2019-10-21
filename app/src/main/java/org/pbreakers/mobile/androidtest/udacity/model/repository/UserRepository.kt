64ad695343920ba9173548f9336c2c9e17276802
Eric Ampire <ericampire.top@gmail.com>
2019-10-21 08:29:34 +0200

    [ADD] The user repository

---
+++ app/src/main/java/org/pbreakers/mobile/androidtest/udacity/model/repository/UserRepository.kt
@@ -0,0 +1,18 @@
+package org.pbreakers.mobile.androidtest.udacity.model.repository
+
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import org.pbreakers.mobile.androidtest.udacity.model.api.UserApi
+import org.pbreakers.mobile.androidtest.udacity.model.dao.UserDao
+
+class UserRepository(private val userApi: UserApi, private val userDao: UserDao) {
+
+    val data = userDao.findAll()
+
+    suspend fun refresh() {
+        withContext(Dispatchers.IO) {
+            val users = userApi.getAll().await()
+            userDao.add(users)
+        }
+    }
+}
