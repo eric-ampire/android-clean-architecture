52ac5f6e311837fbf99d23ed3f6f16679c071d14
Eric Ampire <ericampire.top@gmail.com>
2019-10-21 08:26:00 +0200

    [ADD] UserDao.kt

---
+++ app/src/main/java/org/pbreakers/mobile/androidtest/udacity/model/dao/UserDao.kt
@@ -0,0 +1,15 @@
+package org.pbreakers.mobile.androidtest.udacity.model.dao
+
+import androidx.lifecycle.LiveData
+import androidx.room.Insert
+import androidx.room.Query
+import org.pbreakers.mobile.androidtest.udacity.model.entity.GithubUser
+
+interface UserDao {
+
+    @Query("SELECT * FROM users")
+    fun findAll(): LiveData<List<GithubUser>>
+
+    @Insert
+    fun add(users: List<GithubUser>)
+}
